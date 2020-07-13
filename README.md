74287a2d9455c4e504d8d9dd05245ae5837fc35a
Matti Viskari <viskarimatti@gmail.com>
2020-07-13 13:10:19 +0300

    react js

--- README.md
+++
@@ -1,3 +0,0 @@
-# React
-Repository for react specific sw
-This repository includes React exercises and trials
--- shoppinglist/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Home extends React.Component {
-  render(){
-    return(<p>Welcome {this.props.name} to my shopping list</p>);
-  }
-}
-
-export default Home;
